5833b96bdd7913df42fa3e7e0f1d4809d88931a0
genki <99254762+genki409@users.noreply.github.com>
2022-02-28 21:42:17 +0900

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/sample', function () {
+    return view('sample');
+});
+
+Route::get('/create', function () {
+    return view('posts.create');
+});
+
+Route::get('/edit', function () {
+    return view('posts.edit');
+});
+
+Route::get('/index', function () {
+    return view('posts.index');
+});
+
+Route::get('/show', function () {
+    return view('posts.show');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
